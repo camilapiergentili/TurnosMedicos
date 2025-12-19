65b72475f4792aeb2687aa05c36334741e6b3c68
camilapiergentili <camali_01@hotmail.com>
2025-12-19 15:24:25 -0300

    Saco la ruta de LocalHost en Cors y puse la de Netlify

--- demo/src/main/java/ar/com/dontar/demo/configuration/SecurityConfig.java
+++ demo/src/main/java/ar/com/dontar/demo/configuration/SecurityConfig.java
@@ -107,8 +107,6 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500",
                 "https://effervescent-pavlova-0b97a8.netlify.app"
         ));
 
