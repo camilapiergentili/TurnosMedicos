32b67585cc6b0c38f156d25df2b77702bbaf7464
camilapiergentili <camali_01@hotmail.com>
2025-12-19 01:41:29 -0300

    Realice cambios en la clase de inicializacion

--- demo/src/main/java/ar/com/dontar/demo/initializer/DataInitializer.java
+++ demo/src/main/java/ar/com/dontar/demo/initializer/DataInitializer.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,7 +18,7 @@ public class DataInitializer implements ApplicationListener<ApplicationReadyEven
     private AdministratorRepository administratorRepository;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
 
     @Override
